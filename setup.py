4f126157a0ec10e015f6aefbd8d60a2d8c0cd18a
mweiss17 <martin.weiss@mail.mcgill.ca>
2019-08-11 09:48:20 -0400

    made ocr a module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ocr',
+    packages=find_packages(),
+    version='0.0.1',
+    install_requires=["opencv-python", "pandas", "tqdm", "tables", "numpy", "torch", "torchvision",
+                      "matplotlib", "tensorflow", "tensorboard", "terminaltables", "pillow", "tqdm", "lmdb", "nltk", "natsort"]
+)

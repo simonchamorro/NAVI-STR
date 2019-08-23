525f0cd06dbfc2ee162812330e1fbbacd3b1c2ca
mweiss17 <martin.weiss@mail.mcgill.ca>
2019-08-23 10:53:13 -0400

    film

--- ocr/modules/feature_extraction.py
+++ ocr/modules/feature_extraction.py
@@ -249,7 +249,6 @@ class ResNet(nn.Module):
         x = self.bn0_2(x)
         x = self.relu(x)
         x = self.maxpool1(x)
-        import pdb; pdb.set_trace()
         if type(cond_params) == torch.Tensor and len(cond_params.size()) != 0:
             x, cond_params = self.layer1((x, cond_params))
         else:

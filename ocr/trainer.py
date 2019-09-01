fb83bd5addad3e7cdc462c918fd42622c030f0da
mweiss17 <martin.weiss@mail.mcgill.ca>
2019-09-01 17:13:56 -0400

    pdb

--- ocr/trainer.py
+++ ocr/trainer.py
@@ -241,9 +241,13 @@ def train(opt):
         # Get the list of house number
         labels = [label.split('_')[0] for label in labels]
         if opt.sequential_cond:
-            cond_house_numbers = [get_sequence(
-                int(img_id.split('_')[0]),
-                img_id.split('_')[1], opt.num_cond_hn) for img_id in ids]
+            print(img_id)
+            try:
+                cond_house_numbers = [get_sequence(
+                    int(img_id.split('_')[0]),
+                    img_id.split('_')[1], opt.num_cond_hn) for img_id in ids]
+            except:
+                import pdb; pdb.set_trace()
         else:
             cond_house_numbers = [get_random(
                 int(img_id.split('_')[0]),
